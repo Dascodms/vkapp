376c78e263babaef2783012d3693ac6db0fc2a98
Dascodms <sedachev.m1@gmail.com>
2019-11-22 18:12:37 +0400

    Updates

---
+++ precache-manifest.c31462a79092a3a7abbba74714fd5df5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8fa11665cfdbea90453e46f3598e292",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7744d189d0aafdc22945",
+    "url": "./static/css/2.266626f9.chunk.css"
+  },
+  {
+    "revision": "7744d189d0aafdc22945",
+    "url": "./static/js/2.792f94f3.chunk.js"
+  },
+  {
+    "revision": "d06796893b0dd1af826b",
+    "url": "./static/js/main.a0c04df4.chunk.js"
+  },
+  {
+    "revision": "902345f8190450d029dd",
+    "url": "./static/js/runtime-main.1dd1808a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.3fc12c4d60fcd8373fee983c2219c336.js"
+  "./precache-manifest.c31462a79092a3a7abbba74714fd5df5.js"
 );
 
 self.addEventListener('message', (event) => {
