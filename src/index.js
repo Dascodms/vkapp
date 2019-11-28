0e59a57696494004700b72db1c1b4a77d9c57653
Dascodms <sedachev.m1@gmail.com>
2019-11-28 17:11:21 +0400

    sfsdf

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import connect from '@vkontakte/vk-connect';
 import App from './App';
+import $ from 'jquery'
 // import registerServiceWorker from './sw';
 
 let widget = {
@@ -12,7 +13,7 @@ let widget = {
         "title": "Корюшка",
         "button": "Забронировать",
         "descr": "Петропавловская крепость, д. 3",
-        "cover_id": "club189060637",
+        "cover_id": "7218370_705359",
       },]
 }
 
@@ -26,15 +27,15 @@ connect.subscribe(event => {
    
     const { type, data } = event.detail;
    
-    if (type === 'VKWebAppShowCommunityWidgetPreviewBoxFailed') {
-      // Reading result of the Code Reader
+     if (type === 'VKWebAppShowCommunityWidgetPreviewBoxFailed') {
       console.log(data.code_data, data.error_type, data.error_data);
-    }
+     }
 
-    if(type === 'VKWebAppCallAPIMethod') {
-        console.log(data)
-    }
+    console.log(type)
 
+    if(type === 'VKWebAppCallAPIMethod'){
+        console.data(data)
+    }
 
 
   });
@@ -45,17 +46,18 @@ connect.send("VKWebAppShowCommunityWidgetPreviewBox", {
     "code": `return ${widget};`
 });
 
-
-// fetch(`https://api.vk.com/method/appWidgets.getAppImageUploadServer?image_type=510x128&access_token=ACCESS_TOKEN&v=V
-// `)
-
-connect.send("VKWebAppCallAPIMethod", {"method": "appWidgets.getAppImageUploadServer", "params": {"image_type": "510x128", "v": "5.103", "access_token": "234788bb37091f8c6c4fb83245d5d0c5bc65f660e6bc4b761edb284ff5829c9242281b554d15f3d47cae8"}});
+// const app = '33fe1b19ca2ae196a7a15a2ee0a265161fb58bc03cf547762eba3d817d1261e9a57caa3eab82019631093';
 
 
 
+// {"type":"VKWebAppCallAPIMethodResult","data":{"response":{"upload_url":"https://pu.vk.com/c857624/upload.php?_query=eyJhY3QiOiJhcHBfd2lkZ2V0X2ltYWdlIiwib2lkIjo3MjE4MzcwLCJhaWQiOjcyMTgzNzAsInR5cGUiOjQsIndpZHRoIjoxNTMwLCJoZWlnaHQiOjM4NCwiYXBpX3dyYXAiOnsiaGFzaCI6IjM3YWJiMmEyYzJlNmI5NmM3MCIsImltYWdlIjoie3Jlc3VsdH0ifSwibWlkIjowLCJzZXJ2ZXIiOjg1NzYyNCwiX29yaWdpbiI6Imh0dHBzOlwvXC9hcGkudmsuY29tIiwiX3NpZyI6ImE4YmFiZDQ4ZDAzNjg0MThmOWY0YjZhMjA0NWY0NTg4In0"}}}
 
 
-console.log(JSON.stringify(widget))
+
+
+// {"type":"VKWebAppCallAPIMethodResult","data":{"response":{"id":"7218370_705359","type":"510x128","images":[{"url":"https://sun9-53.userapi.com/c857636/v857636273/10416a/-rlbjcZvQ-g.jpg","width":510,"height":128},{"url":"https://sun9-11.userapi.com/c857636/v857636273/10416b/cZCdfj9yfBs.jpg","width":1020,"height":256},{"url":"https://sun9-51.userapi.com/c857636/v857636273/10416c/ABK1l5_PDyY.jpg","width":1530,"height":384}]}}}
+
+
 
 
 
