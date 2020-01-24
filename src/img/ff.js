99f6804d953e7ecfc3af1652bf324b1ca5f7bf69
Dascodms <sedachev.m1@gmail.com>
2020-01-24 11:04:27 +0400

    delete

--- src/img/ff.js
+++
@@ -1,7 +0,0 @@
-connect.send("VKWebAppShowCommunityWidgetPreviewBox", {
-    "group_id": 189060637, 
-    "type": "text", 
-    "code": `return {
-    \"title\": \"Цитата\",
-    \"text\": \"Текст цитаты\"
-};`});
